0d30b57b05d425f841aea52e5bd3108032a3a909
Shrey K <srkkadia@gmail.com>
2025-08-11 17:03:17 +0530

    Intro to Java

---
+++ Basics/FirstProgram.java
@@ -0,0 +1,15 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+To run this program.
+- use cd(change directory) command to get into specific folder (`cd Basics`)
+- use `javac FirstProgram.java` to compile the program
+- use `java FirstProgram` to run the program
+
+=== Helping cmd commands ===
+1. dir - to listout all the folders in the current directory
+*/
---
+++ Basics/ScanData.java
@@ -0,0 +1,5 @@
+public class ScanData {
+    public static void main(String[] args) {
+        //
+    }
+}
